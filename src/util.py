b953a4bf99a235c6e82ee6ef579c71d0056fa17e
Natsumi H <39241405+NatsumiHB@users.noreply.github.com>
2020-06-19 21:28:43 +0200

    simplified validation of id

--- src/util.py
+++ src/util.py
@@ -6,14 +6,9 @@ waifu_count = 9999  # actual count is waifu_count + 1 but this variable is neede
 # Checks if ID is an integer AND in the bounds
 def validate_id(waifu_id):
     try:
-        waifu_id = int(waifu_id)
+        return 0 <= int(waifu_id) <= waifu_count
     except:
         return False
-
-    if waifu_id > waifu_count or waifu_id < 0:
-        return False
-
-    return True
 
 
 # Returns a random waifu image by id and URL
